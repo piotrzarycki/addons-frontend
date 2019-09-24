b5be456578fb4acab3ff0ad79d31e7ca43c1491b
William Durand <will+git@drnd.me>
2019-09-24 12:32:29 +0200

    Log page views for client-side navigation

--- tests/unit/helpers.js
+++ tests/unit/helpers.js
@@ -1223,8 +1223,10 @@ export function fakeCookies(overrides = {}) {
 
 export const createFakeTracking = (overrides = {}) => {
   return {
+    pageView: sinon.stub(),
     sendEvent: sinon.stub(),
     setDimension: sinon.stub(),
+    setPage: sinon.stub(),
     ...overrides,
   };
 };
